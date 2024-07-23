540141d9098d0521c646adb5d265e2e5f4027f75
Ayumi Aoki Santana <ayumias@motorola.com>
2024-07-23 15:21:59 -0400

    feat: exercicios 5,6 e 7 express

--- ExpTs/src/controllers/main.ts
+++ ExpTs/src/controllers/main.ts
@@ -24,4 +24,51 @@ const lorem = (req: Request, res: Response) => {
   res.send(generateLorem(req.params.paragraphs));
 };
 
-export default { lorem };
+const about = (req: Request, res: Response) => {
+  res.send("Sobre a página!");
+};
+
+const index = (req: Request, res: Response) => {
+  res.send("Oii!");
+};
+
+const hb1 = (req: Request, res: Response) => {
+  res.render("hb1", {
+    mensagem: "Olá, você está aprendendo Express + HBS!",
+    layout: false,
+  });
+};
+
+const hb2 = (req: Request, res: Response) => {
+  res.render("hb2", {
+    poweredByNodejs: true,
+    name: "Express",
+    type: "Framework",
+    layout: false,
+  });
+};
+
+const hb3 = (req: Request, res: Response) => {
+  const profs = [
+    { name: "David Fernandes", room: 1238 },
+    { name: "Horácio Fernandes", room: 1233 },
+    { name: "Edleno Moura", room: 1236 },
+    { name: "Elaine Harada", room: 1231 },
+  ];
+  res.render("hb3", { profs, layout: false });
+};
+
+const hb4 = (req: Request, res: Response) => {
+  const technologies = [
+    { name: 'Express', type: 'Framework', poweredByNodejs: true },
+    { name: 'Laravel', type: 'Framework', poweredByNodejs: false },
+    { name: 'React', type: 'Library', poweredByNodejs: true },
+    { name: 'Handlebars', type: 'Engine View', poweredByNodejs: true },
+    { name: 'Django', type: 'Framework', poweredByNodejs: false },
+    { name: 'Docker', type: 'Virtualization', poweredByNodejs: false },
+    { name: 'Sequelize', type: 'ORM tool', poweredByNodejs: true },
+    ];
+  res.render("hb4", { technologies, layout: false });
+};
+
+export default { lorem, about, index, hb1, hb2, hb3, hb4 };
--- ExpTs/src/index.ts
+++ ExpTs/src/index.ts
@@ -1,16 +1,24 @@
-import express, {Request,Response} from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-
+import { engine } from "express-handlebars";
 import logger from "./middlewares/logger";
-import router from "./router/router"
+import router from "./router/router";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT ?? 3333;
 
+app.engine(
+  "handlebars",
+  engine({ helpers: require(`${__dirname}/views/helpers/helpers.ts`) })
+);
+app.set("view engine", "handlebars");
+app.set("views", `${__dirname}/views`);
+
 app.use(logger("combined"));
+
 app.use(router);
 
 app.listen(PORT, () => {
-    console.log(`Express app iniciada na porta ${PORT}.`);
+  console.log(`Express app iniciada na porta ${PORT}.`);
 });
