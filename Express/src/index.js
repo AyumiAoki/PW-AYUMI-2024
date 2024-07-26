0af1051e6c29fe1e15868056294ecce08d29b2a8
Ayumi Aoki Santana <ayumias@motorola.com>
2024-07-25 23:08:26 -0400

    fix: correcoes no express basico

--- Express/src/index.js
+++ Express/src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3030;
 
 app.get("/", (req, res) => {
     res.send("Hello world!");
